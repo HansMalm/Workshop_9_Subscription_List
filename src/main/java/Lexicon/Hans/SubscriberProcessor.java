616ba83174ca28b121791f3397475b6e8b34acd3
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-04-07 11:49:37 +0200

    mod SubscriberProcessor.java file
    add methods

--- src/main/java/Lexicon/Hans/SubscriberProcessor.java
+++ src/main/java/Lexicon/Hans/SubscriberProcessor.java
@@ -3,5 +3,14 @@ package Lexicon.Hans;
 import java.util.ArrayList;
 
 public class SubscriberProcessor {
-    public ArrayList
+    public ArrayList<Subscriber> findSubscribers (ArrayList<Subscriber> list, SubscriberFilter filter) {
+        ArrayList<Subscriber> resultList = new ArrayList<>();
+        return resultList;
+    }
+
+    public ArrayList<Subscriber> applyToMatching (ArrayList<Subscriber> list,
+                                                  SubscriberFilter filter, SubscriberAction action) {
+        ArrayList<Subscriber> appliedList = new ArrayList<>();
+        return appliedList;
+    }
 }
