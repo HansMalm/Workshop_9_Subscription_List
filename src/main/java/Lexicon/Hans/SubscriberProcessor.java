f0fc42a7dcace23ebc0d553d43646a76ea8df597
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-04-07 12:58:03 +0200

    mod SubscriberProcessor.java file
    bugfix

--- src/main/java/Lexicon/Hans/SubscriberProcessor.java
+++ src/main/java/Lexicon/Hans/SubscriberProcessor.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class SubscriberProcessor {
     public ArrayList<Subscriber> findSubscribers (ArrayList<Subscriber> list, SubscriberFilter filter) {
-        ArrayList<Subscriber> resultList = new ArrayList<>();
+        ArrayList<Subscriber> resultList = list;
         return resultList;
     }
 
     public ArrayList<Subscriber> applyToMatching (ArrayList<Subscriber> list,
                                                   SubscriberFilter filter, SubscriberAction action) {
-        ArrayList<Subscriber> appliedList = new ArrayList<>();
+        ArrayList<Subscriber> appliedList = list;
         return appliedList;
     }
 }
